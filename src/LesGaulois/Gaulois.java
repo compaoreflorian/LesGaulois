ede9fe0dec38a0b16b12e336cefa049201937a3a
compaoreflorian <compaoreeuloge2005@gmail.com>
2026-01-08 10:59:07 +0100

    premier pas en java

---
+++ src/LesGaulois/Gaulois.java
@@ -0,0 +1,18 @@
+package LesGaulois;
+
+public class Gaulois {
+	private String nom;
+	
+	private int force;
+	
+	public Gaulois(String nom, int force) {
+		this.nom=nom;
+		this.force=force;
+	}
+	
+	public String getnom() {
+		return nom;
+	}
+	
+
+}
